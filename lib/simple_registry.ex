6fe976a3c405362a79e8ce3795a64887e0a68170
Zach Liss <zach.liss@dockyard.com>
2019-12-18 15:16:47 -0500

    simple registry as GenServer

---
+++ lib/simple_registry.ex
@@ -0,0 +1,42 @@
+defmodule SimpleRegistry do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  @impl GenServer
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_call({:register, process_key}, caller, state) do
+    case Map.has_key?(state, process_key) do
+      true ->
+        {:reply, :error, state}
+
+      false ->
+        {:reply, :ok, Map.put(state, process_key, caller)}
+    end
+  end
+
+  @impl GenServer
+  def handle_call({:whereis, process_key}, _, state) do
+    case Map.get(state, process_key) do
+      {pid, _} ->
+        {:reply, pid, state}
+
+      nil ->
+        {:reply, nil, state}
+    end
+  end
+
+  def register(process_key) do
+    GenServer.call(__MODULE__, {:register, process_key})
+  end
+
+  def whereis(process_key) do
+    GenServer.call(__MODULE__, {:whereis, process_key})
+  end
+end
